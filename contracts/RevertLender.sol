185cb9f5afa66ebf68ee57925e5dbd5749c0f06d
Aaron Diamond-Reivich <aarondr77@gmail.com>
2019-06-25 23:56:11 -0400

    fixed compilation issues

--- contracts/RevertLender.sol
+++ contracts/RevertLender.sol
@@ -1,9 +1,14 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.5.8;
+
+import "./IERC20.sol";
+import "./SafeMath.sol";
+
 
 contract RevertLender {
+	using SafeMath for uint;
 	// lender -> currency -> upper limit
 	mapping (address => uint) upperLimits;
-  mapping (address => uint)) interestRates;
+  mapping (address => uint) interestRates;
 
 	uint public constant INTEREST_RATE_CONVERTER = 10 ** 7;
 
@@ -14,32 +19,37 @@ contract RevertLender {
 		_;
 	}
 
+	function () external {}
+
 	constructor() public {
 			lender = msg.sender;
 	}
 
 
 	// Allows the lender to update his lending preferences
-	function updateLendingOffer (address currency, uint amount, uint interestRate) onlyLender(msg.sender) public {
-		require(ERC20(currency).transferFrom(msg.sender, address(this), amount), "not approved to transfer tokens");
-		upperLimits[currency] = ERC20(currency).balanceOf(msg.sender);
-		interestRates[msg.sender][currency] = interestRate;
+	function updateLendingOffer (address currency, uint amount, uint interestRate) onlyLender(msg.sender) public payable {
+		require(IERC20(currency).transferFrom(msg.sender, address(this), amount), "not approved to transfer tokens");
+		upperLimits[currency] = IERC20(currency).balanceOf(msg.sender);
+		interestRates[currency] = interestRate;
   }
 
-  function borrow (address smartContract, bytes callData, address borrower, uint principal, address currency) {
+  function borrow (address smartContract, bytes memory callData, address payable borrower, uint principal, address currency) public {
   	require(principal <= upperLimits[currency], "principal > upper limit");
+		uint startingBalance;
+		uint endingBalance;
 		// check if currency is ether
+
 		if (currency == address(0)) {
 			// check the starting balance
-			uint startingBalance = balance(this.address);
+			startingBalance = address(this).balance;
 			smartContract.call.value(principal)(callData);
-			uint endingBalance = balance(this.address);
+			endingBalance = address(this).balance;
 		} else {
 			// check the starting balance
-			uint startingBalance = ERC20(currency).balanceOf(this.address);
-			// Figure out sending money!
+			startingBalance = IERC20(currency).balanceOf(address(this));
+			IERC20(currency).approve(smartContract, principal);
 			smartContract.call(callData);
-			uint endingBalance = ERC20(currency).balanceOf(this.address);
+			endingBalance = IERC20(currency).balanceOf(address(this));
 		}
 
 		// calculate the interest payment due
@@ -47,28 +57,26 @@ contract RevertLender {
 		uint totalAmountDue = principal + totalInterest;
 
 		uint deltaFromTransaction = endingBalance - startingBalance;
-		require(deltraFromTransaction > totalAmountDue, "default on loan");
+		require(deltaFromTransaction > totalAmountDue, "default on loan");
 
+		//if loan was profitable
 		if (currency == address(0)) {
 			borrower.transfer(deltaFromTransaction - totalAmountDue);
 		} else {
-			ERC20(currency).transfer(borrower, deltaFromTransaction - totalAmountDue);
+			IERC20(currency).transfer(borrower, deltaFromTransaction - totalAmountDue);
 		}
   }
 
-    // check if we should revert
-    //unregister
-    function unregister_lender (address smartContract, address lender) {
-    		upperLimits[msg.sender][currency] = 0;
-    }
-
-    //transfer tokens from account
-    function withdraw (address currency, uint amount, address to) onlyLender(msg.sender) public {
-			if (currency == address(0)) {
-				uint startingBalance = this.balance;
-				require(startingBalance >= amount, "insufficient funds to withdraw");
-				to.transfer(currency);
-				require(this.balance + amount = startingBalance, "ether withdraw misbehaved");
-			}
-    }
+  //transfer tokens from account
+  function withdraw (address currency, uint amount, address payable to) onlyLender(msg.sender) public {
+		if (currency == address(0)) {
+			uint startingBalance = address(this).balance;
+			require(startingBalance >= amount, "insufficient funds to withdraw");
+			to.transfer(amount);
+			require(address(this).balance + amount == startingBalance, "ether withdraw misbehaved");
+		} else {
+			require(IERC20(currency).balanceOf(address(this)) >= amount, "insufficient funds to withdraw");
+			require(IERC20(currency).transfer(to, amount), "withdraw transfer failed");
+		}
+  }
 }
